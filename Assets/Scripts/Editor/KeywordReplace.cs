ed4b8d634500be586e54d5d30a97e12d8ab95a0f
alrado <alrado@pragmatix-corp.com>
2017-12-21 15:06:08 +0300

    added template for creation tweet cart by paste from clipboard

--- Assets/Scripts/Editor/KeywordReplace.cs
+++ Assets/Scripts/Editor/KeywordReplace.cs
@@ -22,6 +22,20 @@ public class KeywordReplace : UnityEditor.AssetModificationProcessor {
     fileContent = fileContent.Replace ("#VERSION#", PlayerSettings.bundleVersion);
     fileContent = fileContent.Replace ("#AUTHOR#", PlayerSettings.companyName);
 
+    var startIx = path.LastIndexOf ("/")+1;
+    var endIx = path.LastIndexOf (".");
+    var len = endIx - startIx;
+    var fileName = path.Substring (startIx, len);
+    Debug.LogError ("fileName: " + fileName);
+
+    var clipboard = GUIUtility.systemCopyBuffer;
+    startIx = "class ".Length;
+    endIx = clipboard.IndexOf (":");
+    len = endIx - startIx;
+    clipboard = clipboard.Remove(startIx, len);
+    clipboard = clipboard.Insert(startIx, fileName);
+    fileContent = fileContent.Replace ("#CLIPBOARD#", clipboard);
+
     System.IO.File.WriteAllText (path, fileContent);
     AssetDatabase.Refresh ();
   }
