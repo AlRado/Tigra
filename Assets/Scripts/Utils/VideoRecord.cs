5de883338f02655529b3b4ac9f4ed5e5a1761b46
al_rado <alrado08@gmail.com>
2017-12-31 11:37:33 +0300

    edited VideoRecord

--- Assets/Scripts/Utils/VideoRecord.cs
+++ Assets/Scripts/Utils/VideoRecord.cs
@@ -11,11 +11,11 @@ using UnityEditor.Media;
 #endif
 
 public class VideoRecord : MonoBehaviour {
-  [Range (1, 30)][Tooltip ("Does not changed in runtime")]
-  public int FramesPerSecond = 15;
-  [Range (1, 30)][Tooltip ("Does not changed in runtime")]
+
+  [Range (1, 60)]
   public int RecordTime = 3;
-  //TODO set SoundFreq when there is sound
+
+  private int FramesPerSecond = 60;
   private int SoundFreq = 48000;
 
   private string encodedFilePath;
@@ -47,22 +47,24 @@ public class VideoRecord : MonoBehaviour {
       language = "en"
     };
 
-    int sampleFramesPerVideoFrame = audioAttr.channelCount *
-      audioAttr.sampleRate.numerator / videoAttr.frameRate.numerator;
+    int sampleFramesPerVideoFrame = audioAttr.channelCount * audioAttr.sampleRate.numerator / videoAttr.frameRate.numerator;
 
     var tex = View.Instance.GetScreenTexture ();
     var frames = RecordTime * FramesPerSecond;
-    var deltaTime = (float) 1 / FramesPerSecond;
+    float deltaTime = (float) 1 / FramesPerSecond;
+
+    AudioRenderer.Start ();
 
     using (var encoder = new MediaEncoder (encodedFilePath, videoAttr, audioAttr))
     using (var audioBuffer = new NativeArray<float> (sampleFramesPerVideoFrame, Allocator.Persistent)) {
       for (int i = 0; i < frames; ++i) {
         encoder.AddFrame (tex);
-        // Fill 'audioBuffer' with the audio content to be encoded into the file for this frame.
+        AudioRenderer.Render (audioBuffer);
         encoder.AddSamples (audioBuffer);
         yield return deltaTime;
       }
     }
+    AudioRenderer.Stop ();
     Debug.Log ("Video saved to: " + encodedFilePath);
     EditorUtility.RevealInFinder (encodedFilePath);
   }
